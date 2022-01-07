960ac4d6b89d6d25144a275c56fbcb0d350eac6a
DP <deviprasadp2004@gmail.com>
2022-01-07 09:41:47 +0530

    added bean class

--- OnlineTripManagementSystem/src/main/java/com/cg/otms/entities/User.java
+++ OnlineTripManagementSystem/src/main/java/com/cg/otms/entities/User.java
@@ -1,5 +1,51 @@
 package com.cg.otms.entities;
 
 public class User {
+	private int userId;
+	private String userType;
+	private String password;
+	
+	public User(int userId, String userType, String password) {
+		super();
+		this.userId = userId;
+		this.userType = userType;
+		this.password = password;
+	}
 
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userType=" + userType + ", password=" + password + "]";
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 }
