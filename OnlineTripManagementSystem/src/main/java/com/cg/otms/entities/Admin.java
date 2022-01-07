88fb05c0bb37f87dd7ea0c80f182b6613611177c
DP <deviprasadp2004@gmail.com>
2022-01-07 09:41:34 +0530

    added admin and user bean class

---
+++ OnlineTripManagementSystem/src/main/java/com/cg/otms/entities/Admin.java
@@ -0,0 +1,98 @@
+package com.cg.otms.entities;
+
+public class Admin {
+	private int  adminId;
+	private String adminName;
+	private String password;
+	private String email;
+	private  String mobile;
+	
+	
+	
+	public Admin(int adminId, String adminName, String password, String email, String mobile) {
+		super();
+		this.adminId = adminId;
+		this.adminName = adminName;
+		this.password = password;
+		this.email = email;
+		this.mobile = mobile;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", adminName=" + adminName + ", password=" + password + ", email=" + email
+				+ ", mobile=" + mobile + "]";
+	}
+
+
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getMobile() {
+		return mobile;
+	}
+
+
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+
+
+	public Admin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
---
+++ OnlineTripManagementSystem/src/main/java/com/cg/otms/entities/User.java
@@ -0,0 +1,5 @@
+package com.cg.otms.entities;
+
+public class User {
+
+}
