23d2635e86a7237aecb4eb7fb99cce53f6121d7e
samee <samee@DESKTOP-KPNJ6H3.bbrouter>
2022-01-09 14:35:48 +0530

    added

--- OnlineTripManagementSystem/src/main/java/com/cg/otms/controller/AdminController.java
+++ OnlineTripManagementSystem/src/main/java/com/cg/otms/controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.cg.otms.entities.Admin;
-import com.cg.otms.services.AdminServiceImpl;
 import com.cg.otms.services.IAdminService;
 
 /**
